2d6d89f87602bff0c72cff07fc0b3d5bb8bdde6a
Dave Niewinski <dniewinski@clearpathrobotics.com>
2020-04-27 21:38:17 -0400

    Added duplication scripts and updated bringups

---
+++ ridgeback_gen3_moveit_config/scripts/customize_moveit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ $# == 1 ]
+then
+  echo "Creating Directory <" $PWD"/"$1 ">"
+  mkdir "$1"
+  cd "$1"
+
+  echo "Copying Gen3 Moveit Config"
+  cp -r $(catkin_find ridgeback_gen3_moveit_config)/. .
+  echo "Updating Package"
+  grep -rli 'ridgeback_gen3_moveit_config' * | xargs -i@ sed -i 's/ridgeback_gen3_moveit_config/'$1'/g' @
+  echo "Done"
+
+else
+  echo "USAGE: customize_moveit.sh [new_moveit_package_name]"
+fi
